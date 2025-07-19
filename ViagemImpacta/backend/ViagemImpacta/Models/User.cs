0f51a445549d349120a24663bad65b8723b67f87
Maximus Rosas Burgos <maximus.rosas.burgos@avanade.com>
2025-07-19 11:55:34 -0300

    Fix: Declaração Atributo FirstName

--- ViagemImpacta/backend/ViagemImpacta/Models/User.cs
+++ ViagemImpacta/backend/ViagemImpacta/Models/User.cs
@@ -9,7 +9,7 @@ public class User
     public long UserId { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string FirstName { get; set; 
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Phone { get; set; } //Minimo 10, Max 12
     public int Age { get; set; }
