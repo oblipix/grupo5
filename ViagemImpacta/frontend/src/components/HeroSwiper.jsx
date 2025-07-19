eb48984e3aa497ef8ffa00081cc6bb23069bfece
Maria Carolina <mariacarolinatech@gmail.com>
2025-07-19 18:17:56 -0300

    alteração em funcionalidades no front, seções de hoteis, promocao sendo chamados p a ação de compras, card com valores de pacotes, adionando biblioteca react calendário

--- ViagemImpacta/frontend/src/components/HeroSwiper.jsx
+++ ViagemImpacta/frontend/src/components/HeroSwiper.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // HeroSwiper.jsx
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -5,27 +6,52 @@ import { Autoplay, Pagination, EffectFade, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
+import restauranteImage from '../assets/images/restaurantePE.png';
+import entradaprincipalImage from '../assets/images/entradaprincipalPE.png';
+import piscinaImageRJ from '../assets/images/piscinaRJ.png';
+import cinemalRSImage from '../assets/images/salacinemaRS.png';
+import piscinaGAImage from '../assets/images/piscinaGA.png';
+
 
 function HeroSwiper() {
   const heroSlides = [
+
     {
       id: 1,
-      imageUrl: 'https://picsum.photos/id/1018/1920/800',
-      title: 'Aventura Inesquecível Espera por Você!',
-      subtitle: 'Descubra destinos paradisíacos e paisagens de tirar o fôlego.',
-    },
-    {
-      id: 2,
-      imageUrl: 'https://picsum.photos/id/1040/1920/800',
+      imageUrl: entradaprincipalImage,
       title: 'Sua Próxima Viagem Começa Aqui.',
       subtitle: 'Ofertas exclusivas para explorar o mundo sem sair de casa.',
     },
+
+    {
+      id: 2,
+      imageUrl: restauranteImage,
+      title: 'Aventura Inesquecível Espera por Você!',
+      subtitle: 'Descubra destinos paradisíacos e paisagens de tirar o fôlego.',
+    },
+  
     {
       id: 3,
-      imageUrl: 'https://picsum.photos/id/1084/1920/800',
+      imageUrl: piscinaImageRJ,
       title: 'Relaxe e Desconecte-se.',
       subtitle: 'Pacotes de viagem completos para você aproveitar cada momento.',
     },
+
+    {
+      id: 4,
+      imageUrl: cinemalRSImage,
+      title: 'Entretenimento de Primeira Classe.',
+      subtitle: 'Desfrute de sessões de cinema privativas com toda a qualidade que você merece.',
+    },
+
+    {
+      id: 5,
+      imageUrl: piscinaGAImage,
+      title: 'EOásis de Lazer e Relaxamento.',
+      subtitle: 'Mergulhe em momentos inesquecíveis na nossa piscina, perfeita para toda a família.',
+    },
+
+
   ];
 
   return (
@@ -46,7 +72,7 @@ function HeroSwiper() {
         <SwiperSlide key={slide.id}>
           <div
             className="relative w-full h-full bg-cover bg-center flex items-center justify-start p-8 text-left" 
-            style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.61), rgba(0, 0, 0, 0.53)), url(${slide.imageUrl})` }}
+            style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.81), rgba(0, 0, 0, 0.83)), url(${slide.imageUrl})` }}
           >
             <div className="z-10 max-w-3xl"> 
               {/* === MUDANÇA AQUI: Tamanho do TÍTULO do Hero === */}
