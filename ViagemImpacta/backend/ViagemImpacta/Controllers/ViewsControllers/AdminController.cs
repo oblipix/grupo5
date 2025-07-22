ec6b4095d4d8034205c7a45b6a1c63f3d6ef7b83
Poliana de Barros Brandão <p.de.barros.brandao@avanade.com>
2025-07-22 16:41:42 -0300

    refactor: atualização do UserController

---
+++ ViagemImpacta/backend/ViagemImpacta/Controllers/ViewsControllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViagemImpacta.Controllers.ViewsControllers
+{
+    public class AdminController : Controller
+    {
+        
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
