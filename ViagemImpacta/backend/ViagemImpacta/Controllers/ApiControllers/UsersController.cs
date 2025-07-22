1d78d8f12fa48721de2967fcdb843db828f3840c
Matheus Maia Goulart <matheus.maia.goulart@avanade.com>
2025-07-22 20:52:50 -0300

    Fix: UserController

--- ViagemImpacta/backend/ViagemImpacta/Controllers/ApiControllers/UsersController.cs
+++ ViagemImpacta/backend/ViagemImpacta/Controllers/ApiControllers/UsersController.cs
@@ -11,6 +11,7 @@ namespace ViagemImpacta.Controllers.ApiControllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -22,9 +23,51 @@ namespace ViagemImpacta.Controllers.ApiControllers
             _mapper = mapper; 
         }
 
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> LoginAsync([FromBody] LoginAuthDTO loginAuthDTO)
+        {
+            if (loginAuthDTO == null || string.IsNullOrEmpty(loginAuthDTO.Email) || string.IsNullOrEmpty(loginAuthDTO.Password))
+            {
+                return BadRequest("Email e senha são obrigatórios.");
+            }
+            var user = await _userService.GetUserByEmail(loginAuthDTO.Email);
+            if (user == null)
+            {
+                return NotFound("Usuário não encontrado.");
+            }
+            loginAuthDTO.Role = user.Role.ToString(); // Define a role do usuário no DTO
+
+            if (!BCrypt.Net.BCrypt.Verify(loginAuthDTO.Password, user.Password))
+            {
+                return Unauthorized("Email ou senha inválidos.");
+            }
+            if (!user.Active)
+            {
+                return Unauthorized("Usuário desativado.");
+            }
+            var token = TokensService.GenerateToken(user);
+            if (string.IsNullOrEmpty(token))
+            {
+                return Unauthorized("Falha ao gerar token.");
+            }
+            loginAuthDTO.Password = string.Empty; // Limpa a senha do DTO para segurança
+            return new
+            {
+                token = token,
+                user = new
+                {
+                    UserId = user.UserId,
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Role = user.Role
+
+                }
+            };
+        }
 
         [HttpPost]
-        [Authorize]
         public async Task<ActionResult<UserDTO>> CreateUser([FromBody] CreateUserDTO dto)
         {
             if (!ModelState.IsValid)
@@ -118,50 +161,8 @@ namespace ViagemImpacta.Controllers.ApiControllers
             }
         }
 
-        [HttpPost]
-        [Route("login")]
+        
 
-        public async Task<ActionResult<dynamic>> LoginAsync ([FromBody]LoginAuthDTO loginAuthDTO)
-        {
-            if (loginAuthDTO == null || string.IsNullOrEmpty(loginAuthDTO.Email) || string.IsNullOrEmpty(loginAuthDTO.Password))
-            {
-                return BadRequest("Email e senha são obrigatórios.");
-            }
-            var user = await _userService.GetUserByEmail(loginAuthDTO.Email);
-            if (user == null)
-            {
-                return NotFound("Usuário não encontrado.");
-            }
-            loginAuthDTO.Role = user.Role.ToString(); // Define a role do usuário no DTO
-
-            if (!BCrypt.Net.BCrypt.Verify(loginAuthDTO.Password, user.Password))
-            {
-                return Unauthorized("Email ou senha inválidos.");
-            }
-            if (!user.Active)
-            {
-                return Unauthorized("Usuário desativado.");
-            }
-            var token = TokensService.GenerateToken(user);
-            if (string.IsNullOrEmpty(token))
-            {
-                return Unauthorized("Falha ao gerar token.");
-            }
-            loginAuthDTO.Password = string.Empty; // Limpa a senha do DTO para segurança
-            return new
-            {
-                token = token,
-                user = new
-                {
-                    UserId = user.UserId,
-                    Email = user.Email,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Role = user.Role
-
-                }
-            };
-
-        }
+        
         }
 }
