44ca884c01d13204b634b34189b83854a213c7e3
glorylaflare <juninho_toh@hotmail.com>
2025-07-23 18:50:19 -0300

    refactor: removendo métodos não utilizandos no IUserRepository

--- ViagemImpacta/backend/ViagemImpacta/Repositories/Interfaces/IUserRepository.cs
+++ ViagemImpacta/backend/ViagemImpacta/Repositories/Interfaces/IUserRepository.cs
@@ -6,16 +6,9 @@ namespace ViagemImpacta.Repositories.Interfaces
     public interface IUserRepository : IRepository<User>
     {
         Task<IEnumerable<User>> GetAllClients(int skip, int take);
-
-        Task<User?> GetUserById(int id);
-
-        Task<bool> SetUserDisabled(int id);
-
+        Task<IEnumerable<User>> GetAllEmployees(int skip, int take);
         Task<bool> AlreadyEmailExist(string email);
-
         Task<User?> GetUserByEmail(string email);
-
-        // COPILOT FEZ
         Task<IEnumerable<User>> SearchClientUsers(string search, int skip, int take);
     }
 }
