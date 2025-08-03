7316dcdc953cf65eefff81ab5c4edc9a4392864d
Maria Carolina <mariacarolinatech@gmail.com>
2025-08-02 23:24:11 -0300

    feat: Adicionando nova feeature de novo Co-authored-by: Loy7g <lianthg07@gmail.com>

--- ViagemImpacta/frontend/src/services/AuthService.js
+++ ViagemImpacta/frontend/src/services/AuthService.js
@@ -4,8 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
 
 /**
  * Serviço para integração com a API de autenticação do back-end
- * var nome = carol xD
- * let idade = 1995;
+ * 
  * 
  * 
  */
